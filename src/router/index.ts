8feb1149c042f94fdea719ad9084e84d969cd89b
JoseTobias <tobiassouza0@gmail.com>
2020-05-18 11:06:11 -0300

    Change title nav and vuex

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,52 @@
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
+import Router from 'vue-router';
+import store from '../store';
 
-import TopStories from '../views/TopStories.vue';
-import CodeExamples from '../views/CodeExamples.vue';
-import MyFavorites from '../views/MyFavorites.vue';
+import TopStories from '@/views/TopStories.vue';
+import CodeExamples from '@/views/CodeExamples.vue';
+import MyFavorites from '@/views/MyFavorites.vue';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'top-stories',
-    component: TopStories,
-  },
-  {
-    path: '/code-examples',
-    name: 'code-examples',
-    component: CodeExamples,
-  },
-  {
-    path: '/my-favorites',
-    name: 'my-favorites',
-    component: MyFavorites,
-  },
-];
+class RouteMeta {
+  title: string;
 
-const router = new VueRouter({
+  constructor({title}: { title: string }) {
+    this.title = title;
+  }
+}
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'top-stories',
+      component: TopStories,
+      meta: new RouteMeta({ title: 'Top Stories' })
+    },
+    {
+      path: '/code-examples',
+      name: 'code-examples',
+      component: CodeExamples,
+      meta: new RouteMeta({ title: 'Code Examples' })
+    },
+    {
+      path: '/my-favorites',
+      name: 'my-favorites',
+      component: MyFavorites,
+      meta: new RouteMeta({ title: 'Favorites' })
+    }
+  ]
+});
+
+// This callback runs before every route change, including on initial load
+router.beforeEach((to, from, next) => {
+
+  const routeMeta = to.meta as RouteMeta;
+  store.dispatch('topToolbar/changeTitle', routeMeta.title);
+  next();
 });
 
 export default router;
