8f0ca7f552390d6ca9f838e8534eb79c332a0064
JoseTobias <tobiassouza0@gmail.com>
2020-05-20 10:35:27 -0300

    filtering articles

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -27,6 +27,32 @@ class NewsService {
         return newsArticles;
       });
   }
+  
+  getArticlesByFavorites(): Promise<NewsArticle[]> {
+
+    return fetch('/data/articles.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((serverArticles) => {
+
+        const newsArticles = serverArticles
+          .filter((serverArticle: any) => serverArticle.isFavourite === true)
+          .map((serverArticle: any) => {
+            return {
+              id: serverArticle.id,
+              title: serverArticle.title,
+              content: serverArticle.content,
+              dateString: serverArticle.dateString,
+              baseImageName: serverArticle.baseImageName,
+              articleType: serverArticle.articleType,
+              isFavourite: serverArticle.isFavourite
+            } as NewsArticle;
+          });
+
+        return newsArticles;
+      });
+  }
 }
 
 export default new NewsService();
